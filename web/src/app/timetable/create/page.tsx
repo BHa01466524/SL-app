dc90523b4f334ce7b19241ef676ab4b65d9f4e3a
P Balasubramaniam <bha01466524@devcorptenant.com>
2026-01-28 18:38:04 +0000

    testing routes

---
+++ web/src/app/timetable/create/page.tsx
@@ -0,0 +1,11 @@
+import styles from "../../page.module.css";
+
+export default function CreateTimetablePage() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <h1>Create a new academic timetable</h1>
+      </main>
+    </div>
+  );
+}
---
+++ web/src/app/timetable/page.tsx
@@ -0,0 +1,12 @@
+import styles from "../page.module.css";
+
+export default function TimetableListPage() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <h1>Timetables</h1>
+        <p>View and manage your timetables</p>
+      </main>
+    </div>
+  );
+}
